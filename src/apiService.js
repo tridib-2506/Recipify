ef3dd730d6c30fc241535afa518e723282898793
tridib-2506 <tridib.nandi25062002@gmail.com>
2024-07-26 03:13:13 +0530

    Update apiService.js

--- src/apiService.js
+++ src/apiService.js
@@ -1,14 +1,13 @@
-// src/apiService.js
 export async function fetchRecipe(dish) {
-    try {
-      const response = await fetch(`http://localhost:5000/recipe?dish=${encodeURIComponent(dish)}`);
-      if (!response.ok) {
-        throw new Error('Recipe not found');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching recipe:', error);
-      throw error;
+  try {
+    const response = await fetch(`https://recipify-8j0r.onrender.com/recipe?dish=${encodeURIComponent(dish)}`);
+    if (!response.ok) {
+      throw new Error('Recipe not found');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching recipe:', error);
+    throw error;
   }
+}
